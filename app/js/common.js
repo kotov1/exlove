187a348bab311f316bb2400e820491aa4b568571
Petr Petukhov <kotov0991@gmail.com>
2018-02-08 23:45:50 +0300

    ready for test

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
 $(function() {
 
-	// Custom JS
+	$(".menu-collapsed").click(function() {
+		$(this).toggleClass("menu-expanded");
+	});
+
+	$("#top-menu").on("click","a", function (event) {
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1000);
+	});
+
+
 
 });
